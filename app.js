f1a0d3d3bde0894fdf5e00fd8911d9d7eff3012f
Adachichan <129758854+Adachichan@users.noreply.github.com>
2023-12-12 12:59:36 +0000

    [add] #4 Chapter2_app.js ”文字列を結合する（変数利用）”

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ let hello = 'Hello World';
 alert(hello);
 
 // 文字列の結合
-alert('Hello' + 'World');
+alert('Hello' + 'World!');
+
+// 文字列の結合（変数使用）
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
